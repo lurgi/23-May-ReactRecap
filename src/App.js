36025d8f1ea9cc71fb7ae80460cff96a373b0d94
lurgi <wahoo9040@naver.com>
2023-05-08 20:51:32 +0900

    230508theme

--- src/App.js
+++ src/App.js
@@ -5,48 +5,12 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-`;
-
-const rotationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }50%{
-    transform: rotate(180deg);
-    border-radius: 100px;
-  }100%{
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 80px;
-    }
-  }
+  color: ${(props) => props.theme.textColor};
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
-  return (
-    <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-    </Wrapper>
-  );
+  return <Wrapper>hi</Wrapper>;
 }
 
 export default App;
