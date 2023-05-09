1393d9311fc83be7b62b5a3f8f6e3fda93c7cbd1
lurgi <wahoo9040@naver.com>
2023-05-09 20:13:56 +0900

    230509 JS>TS

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <H1>HI</H1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
